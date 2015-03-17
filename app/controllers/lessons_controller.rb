e37a1a09855ab7ade7cbd6f8647e4ba805b23d36
Alister Fenix <a.fenix@icloud.com>
2015-03-17 13:27:05 -0700

    Display individual lessons on a new page.

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -3,4 +3,9 @@ class LessonsController < ApplicationController
     @lessons = Lesson.all
     render :index
   end
+
+  def show
+    @lesson = Lesson.find(params[:id])
+    render :show
+  end
 end
